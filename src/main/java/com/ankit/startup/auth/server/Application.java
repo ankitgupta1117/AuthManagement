ce9755ed22b69659a1895ed87310687ce8e7dd3d
ankitgupta1117 <ankitgupta1117@gmail.com>
2018-11-15 17:27:28 +0530

    Initial Commit

---
+++ src/main/java/com/ankit/startup/auth/server/Application.java
@@ -0,0 +1,12 @@
+package com.ankit.startup.auth.server;
+
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class Application {
+
+
+}
---
+++ src/main/java/com/ankit/startup/auth/server/config/AuthServer.java
@@ -0,0 +1,9 @@
+package com.ankit.startup.auth.server.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AuthServer {
+
+
+}
